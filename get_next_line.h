a598ed46b1ad7ab7503ac473a5e5ace9b8584b89
Henrique <henrique@usebom.com>
2021-10-18 19:30:31 -0300

    upd: utils functs

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hnass-pe <hnass-pe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 18:50:46 by hnass-pe          #+#    #+#             */
-/*   Updated: 2021/10/13 18:55:21 by hnass-pe         ###   ########.fr       */
+/*   Updated: 2021/10/18 19:29:24 by hnass-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: hnass-pe <hnass-pe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 18:50:54 by hnass-pe          #+#    #+#             */
-/*   Updated: 2021/10/13 18:50:57 by hnass-pe         ###   ########.fr       */
+/*   Updated: 2021/10/18 19:29:13 by hnass-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,4 +99,30 @@ char	*ft_strdup(const char *s)
 	}
 	str[i] = '\0';
 	return (str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*p;
+	size_t	i;
+
+	i = ft_strlen(s);
+	if (start < i)
+	{
+		if (i > len)
+			p = (char *) malloc(sizeof(char) * len + 1);
+		else
+			p = (char *) malloc(sizeof(char) * i + 1);
+		if (p == NULL)
+			return (NULL);
+		ft_strlcpy(p, &s[start], len + 1);
+	}
+	else
+	{
+		p = (char *) malloc(sizeof(char) * 1);
+		if (p == NULL)
+			return (NULL);
+		ft_strlcpy(p, "", 1);
+	}
+	return (p);
 }
